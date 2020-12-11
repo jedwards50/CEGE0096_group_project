f742c036d7d26869ee7228468645763f4e1ad072
niamhfrench <niamhfrench@hotmail.co.uk>
2020-12-11 14:31:14 +0000

    Feat: Give user option to re-enter co-ordinates if the original co-ordinates are outside of testing area.

--- draft - task 1 & 2 - Hongmin Du.py
+++ draft - task 1 & 2 - Hongmin Du.py
@@ -9,20 +9,17 @@
 
 # 0.1 - Import required packages for the following steps
 import os
-import rasterio
-from shapely.geometry import Point
-from shapely.geometry import Polygon
 
-elevation_radius = 5000
+import rasterio
+from rasterio import windows
+from shapely.geometry import Point, Polygon
 
 # 0.2 - Title for the software and its background:
 print("**** CEGE0096 Assignment 2 - Flood Emergency Planning - Dragonfly (0.1) ****")
-print()
 print("Extreme flooding is expected on the Isle of Wight and the authority in charge of planning the "
-      "emergency response is advising everyone to proceed by foot to the nearest high ground.")
-print("This software will give the quickest route from the user's current location to the highest point "
+      "emergency response is advising everyone to proceed by foot to the nearest high ground."
+      "This software will give the quickest route from the user's current location to the highest point "
       "within a 5km radius in the chosen area.")
-print()
 
 # Task 1. User Input.
 # In this task, this software should:
@@ -32,17 +29,29 @@ print()
 # 1.0 - Introduction
 print("Firstly, let's check whether your location is included in the testing area!")
 
+
 # 1.1 - Let the user input the easting and northing and test whether the point is in the testing area
 # Notice: the easting and northing should follow the style of British National Grid
-p = Point(float(input('Please input the value of your easting (use British National Grid): ')),
-          float(input('Please input the value of your northing (use British National Grid): ')))
+
+def input_easting_and_northing():
+    easting = float(input('Please input the value of your easting (use British National Grid): '))
+    northing = float(input('Please input the value of your northing (use British National Grid): '))
+    return easting, northing
+
+
+p = Point(input_easting_and_northing())
 
 testing_area = Polygon([(430000, 80000), (430000, 95000), (465000, 95000), (465000, 80000)])
 
 if p.within(testing_area):
     print("This location can be tested by this software!")
 else:
-    print("This location is outside the testing area! Please enter another location!")
+    print("This location is outside the testing area!")
+    quit_or_reenter = input("Type 'q' to quit or 'r' to re-enter.")
+    if quit_or_reenter == 'q':
+        quit()
+    elif quit_or_reenter == 'r':
+        p = Point(input_easting_and_northing())
 
 # Task 2. Highest Point Identification.
 # In this task, this software should:
@@ -55,4 +64,7 @@ print("Secondly, let's find the highest point within a 5km radius from the user 
 # NB: Material folder must be stored in working directory. A .gitignore file can be used to ignore this folder
 # when pushing to GitHub. The Material folder cannot be uploaded to GitHub because file sizes are greater than 100MB.
 elevation_asc = rasterio.open(os.path.join('Material', 'elevation', 'SZ.asc'))
-elevation_array = elevation_asc.read(1)
+elevation_array = elevation_asc.read(1,
+                                     window=windows.from_bounds((p.x - 5000), (p.y - 5000), (p.x + 5000), (p.y + 5000)))
+# Create buffer around point.
+buffer_5km = p.buffer(5000)
